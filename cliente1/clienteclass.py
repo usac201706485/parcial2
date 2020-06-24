3023501218d69cdea9afb4d1217e07fdf7315b4b
usac201706485 <3304173711202@ingenieria.usac.edu.gt>
2020-06-23 22:01:26 -0600

    Completo Version.1

--- cliente1/clienteclass.py
+++ cliente1/clienteclass.py
@@ -1,4 +1,3 @@
-#OOP PARA LA TRANSMISION DE MENSAJES DE TEXT
 import paho.mqtt.client as paho
 import logging
 from brokerData import * #MANU Informacion de la conexion
@@ -20,18 +19,24 @@ def on_publish(client, userdata, mid):
 
 #MANU Callback que se ejecuta cuando llega un mensaje al topic suscrito
 def on_message(client, userdata, msg):
+    from datetime import datetime
     #MANU Se muestra en pantalla informacion que ha llegado
     logging.info("Fuente: " + str(msg.topic))
     firstopic = '{:.5}'.format(str(msg.topic))
-    if firstopic == 'audio':
-        #logging.info("\n1 Mensaje de Audio Recibido: " + str(msg.payload))
-        archivo = open('copia.wav','wb') #Abrir para SOBREESCRIBIR el archivo existente
+    if firstopic == 'audio':#AAMS Se compara si el mensaje llegado fue de Audio o Texto
+        now = datetime.now()
+        name_ar = str(int(datetime.timestamp(now))) + '.wav' #AAMS Nombre del archivo de audio para el receptor del mismo
+        archivo = open(name_ar,'wb') #AAMS Abrir para SOBREESCRIBIR el archivo existente
         a = msg.payload
-        archivo.write(a)
-        archivo.close() #Siempre cerrar el archivo al finalizar la escritura
-        os.system('aplay copia.wav')
+        archivo.write(a) #AAMS Sobreescribiendo el archivo de audio
+        archivo.close() #AAMS Siempre cerrar el archivo al finalizar la escritura
+        escuchar = 'aplay ' + name_ar
+        os.system(escuchar) #AAMS Reproduccion del archivo de audio
+        print("ENVIAR TEXTO = 1 \nENVIAR AUDIO = 2 \nSeleccion:")
     else:
         logging.info("\n1 Mensaje Recibido: " + str(msg.payload))
+        print("ENVIAR TEXTO = 1 \nENVIAR AUDIO = 2 \nSeleccion:")
+    
 client = paho.Client(clean_session=True) #MANU Nueva instancia de cliente
 client.on_connect = on_connect #MANU Se configura la funcion "Handler" cuando suceda la conexion
 client.on_message = on_message #MANU Se configura la funcion "Handler" que se activa al llegar un mensaje a un topic subscrito
@@ -39,18 +44,18 @@ client.on_publish = on_publish #MANU Se configura la funcion "Handler" que se ac
 client.username_pw_set(MQTT_USER, MQTT_PASS) #MANU Credenciales requeridas por el broker
 client.connect(host=MQTT_HOST, port = MQTT_PORT) #MANUConectar al servidor remoto
 
-def publishData(topicRoot, topicName, value, qos = 0, retain = False):
+def publishData(topicRoot, topicName, value, qos = 0, retain = False): #MANU Funcion para recibir topics y publicarlos
     topic = topicRoot + "/" + topicName
     client.publish(topic, value, qos, retain)
 
-def fileReadU(fileName = 'usuario'):
+def fileReadU(fileName = 'usuario'):#MANU leer el archivo de texto plano
     archivo = open(fileName,'r') #MANU Abrir el archivo en modo de LECTURA
     for line in archivo: #MANU Leer cada linea del archivo
-        usuario = (line) #MANU Imprimir el texto de USUARIO
+        usuario = (line) #MANU Guardar el texto de USUARIO
     return usuario
     archivo.close()
 
-def fileReadS(fileName = 'salas'):
+def fileReadS(fileName = 'salas'):#MANU leer el archivo de texto plano
     archivo = open(fileName,'r') #MANU Abrir el archivo en modo de LECTURA
     sala = []
     for line in archivo: #MANU Leer cada linea del archivo
@@ -59,49 +64,63 @@ def fileReadS(fileName = 'salas'):
     archivo.close()
 
 
-def fileReadAU(fileName = 'prueba.wav'):
+def fileReadAU(fileName = 'prueba.wav'):#AAMS Creacion el archivo de audio local
     tiempo = input('Duracion: ')
-    comando = 'arecord -d' + tiempo + ' -f U8 -r 8000 prueba.wav'
-    logging.info('Comenzando grabacion')
-    os.system(comando)
-    logging.info('Grabacion finalizada, inicia reproduccion')
-    archivo = open(fileName,'rb') #Abrir el archivo en modo de LECTURA
-    for line in archivo: #Leer cada linea del archivo
-        a = line #Imprimir linea por linea del archivo leido
-    return a
-    archivo.close() #Cerrar el archivo al finalizar
-    
-
-class   texto(object):
-    def __init__(self, MENU1):
+    if int(tiempo) > 30:#AAMS Limitacion del tiempo a solo 30 segundos maximo
+        print('El mensaje No debe ser mayor a 30 Segundos')
+    else:
+        comando = 'arecord -d' + tiempo + ' -f U8 -r 8000 prueba.wav'
+        logging.info('Comenzando grabacion')
+        os.system(comando)
+        logging.info('Grabacion finalizada')
+        archivo = open(fileName,'rb') #AAMS Abrir el archivo en modo de LECTURA
+        for line in archivo: #AAMS Leer cada linea del archivo
+            a = line #AAMS Guardar el texto del archivo leido
+        return a
+        archivo.close() #AAMS Cerrar el archivo al finalizar
+        
+#OOP[PARA CLIENTES]
+class   texto(object): #MANU CLASE PARA EL MANEJO DE LA INTERFAZ DEL CLIENTE
+    def __init__(self, MENU1):#MANU CONSTRUCTOR
         self.MENU1 = str(MENU1)
 
 
     
     def menu(self):
-        if str(self.MENU1) == '1':
+        estado = 0
+        if str(self.MENU1) == '1':#AAMS El usuario desea enviar texto
             MENU2 = input("ENVIAR A USUARIO = 1 \nENVIAR A SALA = 2 \nSeleccion:\n")
-            if MENU2 == '1':
+            if MENU2 == '1': # AAMS el Usuario desea enviar texto a un usuario
                 DESTINO = input("Destinatario/Usuario: \n")
                 texto_enviar = input("Escribir mensaje: \n")
                 publishData(USUARIOS, GRUPO + "/" + DESTINO , texto_enviar)
                 logging.info("Mensaje enviado\n")
-            if MENU2 == '2':
+            if MENU2 == '2':# AAMS el usuario desea enviar texto a una sala
                 DESTINO = input("Destinatario/#Sala: \n")
                 texto_enviar = input("Escribir mensaje: \n")
                 publishData(SALAS, GRUPO + "/" + DESTINO , texto_enviar)
                 logging.info("Mensaje enviado\n")
-        if str(self.MENU1) == '2':
+            estado = 0 #AAMS El while principal seguira funcionando
+            return estado
+        if str(self.MENU1) == '2': # AAMS El usuario desea enviar un audio
             MENU2 = input("ENVIAR A USUARIO = 1 \nENVIAR A SALA = 2 \nSeleccion:\n")
-            if MENU2 == '1':
+            if MENU2 == '1':#AAMS El usuario desea enviar un audio a un usuario
                 DESTINO = input("Destinatario/Usuario: \n")
                 textob = fileReadAU()
-                publishData(AUDIO, GRUPO + "/" + DESTINO , textob)
-                logging.info("Mensaje enviado\n")
-            if MENU2 == '2':
+                if textob != None:
+                    publishData(AUDIO, GRUPO + "/" + DESTINO , textob)
+                    logging.info("Mensaje enviado\n")
+            if MENU2 == '2':#AAMS El usuario desea enviar un audio a una sala
                 DESTINO = input("Destinatario/#Sala: \n")
                 textob = fileReadAU()
                 publishData(AUDIO, GRUPO + "/" + DESTINO , textob)
                 logging.info("Mensaje enviado\n")
+            estado = 0#AAMS El while principal seguira funcionando
+            return estado
+        if str(self.MENU1) == '3':#AAMS El usuario desea cerrar su sesion
+            estado = 1#AAMS El while principal dejara de funcionar
+            return estado
+            
+            
 
    
--- cliente3/cliente.py
+++ cliente3/cliente.py
@@ -4,7 +4,8 @@ import time
 from brokerData import * #MANU Informacion de la conexion
 from clienteclass import * #MANU Clase manejo de Clientes
 
-sala = fileReadS()
+#MANU Lectura truncada de los textos planos 
+sala = fileReadS()  
 usuario = '{:.9}'.format(fileReadU())
 
 #MANU Configuracion inicial de logging
@@ -13,19 +14,19 @@ logging.basicConfig(
     format = '[%(levelname)s] (%(processName)-10s) %(message)s'
     )
 
-#MANU Tiempo de espera 
+#MANU Tiempo de espera [para mostrar el menu de una forma mas ordenada]
 DEFAULT_DELAY = 1 #MANU segundos
 
 logging.info("Sesion en Linea") #MANU Mensaje en consola
 
 #MANU Nos conectaremos a distintos topics:
 qos = 2
-#MANU Subscripcion simple con tupla (topic,qos)
+#MANU Subscripcion a los topics segun el texto plano de USUARIO
 subID = "usuarios/27/" + str(usuario)
 subAU = "audio/27/" + str(usuario)
 client.subscribe((subID , qos))
 client.subscribe((subAU , qos))
-
+#Manu Subscripcion a los topics segun el texto plano de SALAS
 for i in range(len(sala)):
     subSala = "salas/27/" + str(sala[i])
     subAUSala = "audio/27/" + str(sala[i])
@@ -33,17 +34,18 @@ for i in range(len(sala)):
     client.subscribe((subAUSala, qos))
 #MANU Iniciamos el thread (implementado en paho-mqtt) para estar atentos a mensajes en los topics subscritos
 client.loop_start()
-
+estado = 0
 #MANU Loop principal 
 try:
-    while True:
+    while estado == 0:
         time.sleep(DEFAULT_DELAY)    #MANU Retardo hasta la proxima publicacion de info
-        MENU1 = texto(input("ENVIAR TEXTO = 1 \nENVIAR AUDIO = 2 \nSeleccion:\n"))
-        MENU1.menu()
+        MENU1 = texto(input("ENVIAR TEXTO = 1 \nENVIAR AUDIO = 2 \nSALIR = 3 \nSeleccion:\n"))
+
+        estado = MENU1.menu()
         
 except KeyboardInterrupt:
-    logging.warning("Desconectando del broker MQTT...")
+    logging.warning("Desconectando del broker MQTT...") #MANU al ejecutar ^C el programa se cierra
 
 finally:
-    client.disconnect()
+    client.disconnect() #MANU desconexion del broker
     logging.info("Se ha desconectado del broker. Saliendo...")
